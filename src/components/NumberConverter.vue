f7840aa56fc603b60de001bc585d92ff4fb9b107
MFukazawa <micahfukazawa@gmail.com>
2021-05-14 21:08:47 +0900

    Add: thousands place. Update hundreds place bugs

--- src/components/NumberConverter.vue
+++ src/components/NumberConverter.vue
@@ -15,11 +15,15 @@ export default defineComponent({
     const number = ref('');
 
     const numberFormatted = computed(() => {
-      const numbers = /[0-9]+$/;
-      const containsOnlyNumbers = numbers.test(number.value);
+      const rounded = Math.round(parseInt(number.value));
+
+      return parseInt(rounded.toString().replace(/\D/g,''));
+      // const numbers = /[0-9]+$/;
+      // const containsOnlyNumbers = numbers.test(number.value);
 
       // TODO get region dynamically
-      return containsOnlyNumbers ? new Intl.NumberFormat().format(parseInt(number.value)) : -1
+      // return containsOnlyNumbers ? new Intl.NumberFormat().format(parseInt(number.value)) : -1
+      // return containsOnlyNumbers ? parseInt(number.value) : -1
     });
 
     const numberArray = computed(() => {
@@ -36,10 +40,14 @@ export default defineComponent({
 
     const hundredsPlace = computed(() => {
       return numberArray.value[numberArray.value.length - 3];
-    })
+    });
+
+    const thousandsPlace = computed(() => {
+      return numberArray.value[numberArray.value.length - 4];
+    });
 
     const convertedNumber = computed(() => {
-      if (numberFormatted.value === 'NaN' || numberFormatted.value === -1) {
+      if (numberFormatted.value === NaN || numberFormatted.value === -1) {
         return 'Please enter a number'
       } else {
         let converted = [] as Array<string>;
@@ -62,10 +70,28 @@ export default defineComponent({
           12: 'byaku',
           13: 'ppyaku',
           14: 'yon',
-          15: 'ro'
+          15: 'ro',
+          16: 'sen',
+          17: 'zen',
+          18: 'has',
+          19: 'ha',
         }
 
         const romajiKeys = Object.keys(romaji).map((e) => parseInt(e));
+
+        if (thousandsPlace.value) {
+          const thousandsPlaceValue = romajiKeys.find((e) => e === thousandsPlace.value) || -1;
+
+          if (thousandsPlaceValue === 1) {
+            converted.push(romaji[16]);
+          } else if (romaji[thousandsPlaceValue] === 'san') {
+            converted.push(romaji[thousandsPlaceValue], romaji[17]);
+          } else if (romaji[thousandsPlaceValue] === 'hachi') {
+            converted.push(romaji[18], romaji[16]);
+          } else {
+            converted.push(romaji[thousandsPlaceValue], romaji[16]);
+          }
+        };
 
         if (hundredsPlace.value) {
           const hundredsPlaceValue = romajiKeys.find((e) => e === hundredsPlace.value) || -1;
@@ -76,11 +102,13 @@ export default defineComponent({
             converted.push(romaji[hundredsPlaceValue], romaji[12]);
           } else if (romaji[hundredsPlaceValue] === 'roku') {
             converted.push(romaji[15], romaji[13]);
+          } else if (romaji[hundredsPlaceValue] === 'hachi') {
+            converted.push(romaji[19], romaji[13]);
           }
           else {
             converted.push(romaji[hundredsPlaceValue], romaji[11]);
           }
-        }
+        };
 
         if (tensPlace.value) {
           const tensPlaceValue = romajiKeys.find((e) => e === tensPlace.value) || -1;
@@ -88,8 +116,6 @@ export default defineComponent({
           if (tensPlaceValue === 1) {
             converted.push(romaji[10]);
           } else {
-            // TODO add 40 series with 'yon'
-            // TODO add 70 series with 'nana'
             converted.push(romaji[tensPlaceValue], romaji[10]);
           }
         };
