63176ea9c450f431f23da833160022f0149928e7
MFukazawa <micahfukazawa@gmail.com>
2021-05-16 21:06:16 +0900

    Fixed some bugs around 100000 type numbers up to ten-hundred thousand.

--- src/components/NumberConverter.vue
+++ src/components/NumberConverter.vue
@@ -2,8 +2,15 @@
   <div class="number-input">
     <label for="number">Enter a number</label>
     <input type="text" id="number" name="number" v-model="number">
+    <!-- TODO don't display if 0 -->
     <p>{{ numberWithCommas }}</p>
-    <p v-if="number.length !== 0">{{ convertedNumber }}</p>
+    <p v-if="parseInt(number) !== 0">
+      <span
+        v-for="number in convertedNumber"
+        :key="number"
+        :class="{ 'large-unit': isLargeUnit(number) }"
+      >{{ number }}</span>
+    </p>
   </div>
 </template>
 
@@ -48,6 +55,8 @@ export default defineComponent({
 
     const hundredMillionsPlace = computed(() => numberArray.value[numberArray.value.length - 9]);
 
+    const billionsPlace = computed(() => numberArray.value[numberArray.value.length - 10]);
+
     const convertedNumber = computed(() => {
       if (numberFormatted.value === NaN || numberFormatted.value === -1) {
         return 'Please enter a number'
@@ -79,19 +88,38 @@ export default defineComponent({
           19: 'ha',
           20: 'man',
           21: 'oku',
+          22: 'is'
         }
 
         const romajiKeys = Object.keys(romaji).map((e) => parseInt(e));
 
-        // TODO
-        // if (hundredMillionsPlace.value) {
-          
-        // }
+        if (billionsPlace.value) {
+          const billionsPlaceValue = romajiKeys.find((e) => e === billionsPlace.value) || -1;
 
+          if (billionsPlaceValue === 1) {
+           converted.push(romaji[10], romaji[21]);
+          } else if (billionsPlaceValue && tenThousandsPlace.value === 0) {
+            converted.push(romaji[billionsPlaceValue], romaji[10], romaji[20])
+          } else {
+            converted.push(romaji[billionsPlaceValue], romaji[10])
+          }
+        };
+
+        if (hundredMillionsPlace.value) {
+          const hundredMillionsPlaceValue = romajiKeys.find((e) => e === hundredMillionsPlace.value) || -1;
+
+          if (hundredMillionsPlaceValue) {
+            converted.push(romaji[hundredMillionsPlaceValue], romaji[21])
+          }
+        };
+
+        // 8 digit sen man
         if (tenMillionsPlace.value) {
           const tenMillionsPlaceValue = romajiKeys.find((e) => e === tenMillionsPlace.value) || -1;
 
-          if (tenMillionsPlaceValue === 1) {
+          if (tenMillionsPlaceValue === 1 && millionsPlace.value === 0) {
+            converted.push(romaji[22], romaji[16]);
+          } else if (tenMillionsPlaceValue === 1 && millionsPlace.value !== 0) {
             converted.push(romaji[16]);
           } else if (romaji[tenMillionsPlaceValue] === 'san') {
             converted.push(romaji[tenMillionsPlaceValue], romaji[17]);
@@ -100,44 +128,70 @@ export default defineComponent({
           } else {
             converted.push(romaji[tenMillionsPlaceValue], romaji[16]);
           }
-        }
+        };
 
-        if (millionsPlace.value) {
+        // 7 digit hyaku man
+        if (millionsPlace.value || tenMillionsPlace.value && millionsPlace.value === 0) {
           const millionsPlaceValue = romajiKeys.find((e) => e === millionsPlace.value) || -1;
 
-          if (millionsPlaceValue === 1) {
-            converted.push(romaji[11]);
-          } else if (millionsPlaceValue && hundredThousandsPlace.value === 0) {
-            converted.push(romaji[millionsPlaceValue], romaji[11], romaji[20])
-          } else if (romaji[millionsPlaceValue] === 'san') {
+          // TODO 3748293 is ALL fucked up
+
+          if (millionsPlaceValue === -1) {
+            converted.push(romaji[20]);
+          }
+          // hyaku man
+          else if (millionsPlaceValue === 1 && hundredThousandsPlace.value === 0) {
+            converted.push(romaji[11], romaji[20]);
+          }
+          // hyaku ni juu man
+          else if (millionsPlaceValue === 1 && hundredThousandsPlace.value !== 0) {
+            converted.push(romaji[11], )
+          }
+          // special reading
+          else if (romaji[millionsPlaceValue] === 'san') {
             converted.push(romaji[millionsPlaceValue], romaji[12]);
-          } else if (romaji[millionsPlaceValue] === 'roku') {
+          }
+          // special reading
+          else if (romaji[millionsPlaceValue] === 'roku') {
             converted.push(romaji[15], romaji[13]);
-          } else if (romaji[millionsPlaceValue] === 'hachi') {
+          }
+          // special reading
+          else if (romaji[millionsPlaceValue] === 'hachi') {
             converted.push(romaji[19], romaji[13]);
-          } else {
+          }
+          else {
             converted.push(romaji[millionsPlaceValue], romaji[11]);
           }
         };
 
+        // 6 digit juu man
         if (hundredThousandsPlace.value) {
           const hundredThousandsPlaceValue = romajiKeys.find((e) => e === hundredThousandsPlace.value) || -1;
 
+          // juu
           if (hundredThousandsPlaceValue === 1) {
             converted.push(romaji[10]);
-          } else if (hundredThousandsPlaceValue && tenThousandsPlace.value === 0) {
-            converted.push(romaji[hundredThousandsPlaceValue], romaji[10], romaji[20])
-          } else {
+          }
+          // kyuu juu
+          else {
             converted.push(romaji[hundredThousandsPlaceValue], romaji[10])
           }
         };
 
-        if (tenThousandsPlace.value) {
+        // 5 digit man
+        if (tenThousandsPlace.value || hundredThousandsPlace.value && tenThousandsPlace.value === 0) {
           const tenThousandsPlaceValue = romajiKeys.find((e) => e === tenThousandsPlace.value) || -1;
 
-          converted.push(romaji[tenThousandsPlaceValue], romaji[20]);
+          // yon man
+          if (tenThousandsPlaceValue !== -1) {
+            converted.push(romaji[tenThousandsPlaceValue], romaji[20]);
+          } else {
+            // man
+            converted.push(romaji[20]);
+          }
         };
 
+        // 4 digit sen
         if (thousandsPlace.value) {
           const thousandsPlaceValue = romajiKeys.find((e) => e === thousandsPlace.value) || -1;
 
@@ -152,6 +206,7 @@ export default defineComponent({
           }
         };
 
+        // 3 digit hyaku
         if (hundredsPlace.value) {
           const hundredsPlaceValue = romajiKeys.find((e) => e === hundredsPlace.value) || -1;
 
@@ -168,6 +223,7 @@ export default defineComponent({
           }
         };
 
+        // 2 digit juu
         if (tensPlace.value) {
           const tensPlaceValue = romajiKeys.find((e) => e === tensPlace.value) || -1;
 
@@ -178,18 +234,26 @@ export default defineComponent({
           }
         };
 
+        // 1 digit 1-9
         if (onesPlace.value) {
           const onesPlaceValue = romajiKeys.find((e) => e === onesPlace.value) || -1;
 
           converted.push(romaji[onesPlaceValue]);
         };
 
-        return converted.join('・');
+        console.log(converted)
+
+        return converted;
       }
     });
 
+    const isLargeUnit = (value: string) => {
+      const largeUnits = ['oku', 'man', 'sen', 'zen', 'hyaku', 'byaku', 'ppyaku'];
 
-    return { number, numberWithCommas, convertedNumber, onesPlace }
+      return largeUnits.includes(value);
+    }
+
+    return { number, numberWithCommas, convertedNumber, onesPlace, isLargeUnit }
   }
 })
 </script>
@@ -203,6 +267,16 @@ input[type=text] {
 }
 
 p {
+  display: flex;
+  flex-wrap: wrap;
   font-size: 3em;
+}
+
+p span {
+  padding: 0 10px;
+}
+
+.large-unit {
+  font-weight: bold;
 }
 </style>
